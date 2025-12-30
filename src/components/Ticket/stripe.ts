e5abc493e6e6f0e231a01cad948ca56de3a0c953
Wojtek Erbetowski <wojtek@erbetowski.pl>
2025-12-30 18:25:09 +0100

    Use env var for Stripe key

--- src/components/Ticket/stripe.ts
+++ src/components/Ticket/stripe.ts
@@ -1,4 +1,4 @@
-
 import { loadStripe } from "@stripe/stripe-js";
-const VITE_STRIPE_PUBLISHABLE_KEY = "pk_test_51Q6ELFIbapYzGEkCui2t5NyVJG6LkaZW3wZW9y2YBs1uewMXut1X8m1aGueZjAiY0XgMSwIQ2qndKmTHHl15UTqQ00tmqGPnBh";
+
+const VITE_STRIPE_PUBLISHABLE_KEY = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY;
 export const stripePromise = loadStripe(VITE_STRIPE_PUBLISHABLE_KEY);
